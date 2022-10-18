c01069e9626ecc7e0ae91a6403c076c1f64a8026
2130554h <114969240+2130554h@users.noreply.github.com>
2022-10-18 11:04:30 +0900

    change section 3-2&3-3

--- section3-3/sketch.js
+++ section3-3/sketch.js
@@ -1,22 +1,41 @@
 // テキスト「キーボード操作に反応する」
-let x, y;
+let x, y, vy;
+const g = 1;
+const jump = 20;
+
 
 function setup(){
   createCanvas(windowWidth, windowHeight);
   x = width / 2;
   y = height / 2;
+  vy = 0;
 }
 
 function draw(){
   background(160, 192, 255);
   ellipse(x, y, 50);
+
+  y += vy;
+
+  if(y < height/2){ vy += g }
+  else{
+    vy += 0;
+    y = height/2}
+    
   if(keyIsDown(LEFT_ARROW)){ x -= 5; }
   if(keyIsDown(RIGHT_ARROW)){ x += 5; }
-  if(keyIsDown(UP_ARROW)){ y -= 5; }
-  if(keyIsDown(DOWN_ARROW)){ y += 5; }
-  if(keyIsDown("A".charCodeAt(0))){ x+= 10; }
-  if(keyIsDown(" ".charCodeAt(0))){ x-= 10; }
+  //if(keyIsDown(UP_ARROW)){ y -= 5; }
+  //if(keyIsDown(DOWN_ARROW)){ y += 5; }
+  //if(keyIsDown("A".charCodeAt(0))){ x+= 10; }
+  //if(keyIsDown(" ".charCodeAt(0))){ x-= 10; }
 }
+
+function keyPressed(){
+  if(key == " "){
+  vy = -jump
+  }
+}
+
 
 // イベントハンドラを使用するパターン
 // function keyPressed(){
